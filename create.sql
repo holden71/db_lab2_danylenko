27fda1f715a68abb9f97d9e394eb0a33fd468761
Nick Holden <52251652+holden71@users.noreply.github.com>
2021-10-28 21:22:08 +0300

    Update create.sql

--- create.sql
+++ create.sql
@@ -13,6 +13,13 @@ CREATE TABLE Products
   product_price    int       NOT NULL 
 );
 
+CREATE TABLE Rating
+(
+  product_barcode   char(50)  NOT NULL,
+  competition_year  int  NOT NULL,
+  product_position  int       NOT NULL
+);
+
 CREATE TABLE Spendings 
 (
   customer_id      char(10)  NOT NULL,
@@ -20,11 +27,11 @@ CREATE TABLE Spendings
   spent_money      int       NOT NULL
 );
 
-
 --Команди налаштування первинних та зовнішніх ключів:
 ALTER TABLE Customers ADD CONSTRAINT PK_Customers PRIMARY KEY (customer_id);
 ALTER TABLE Products ADD CONSTRAINT PK_Products PRIMARY KEY (product_barcode);
 ALTER TABLE Spendings ADD CONSTRAINT PK_Spendings PRIMARY KEY (customer_id, product_barcode);
+ALTER TABLE Rating ADD CONSTRAINT PK_Rating PRIMARY KEY (product_barcode, competition_year);
 
 ALTER TABLE Spendings ADD CONSTRAINT FK_Spendings_Customers FOREIGN KEY (customer_id) REFERENCES Customers (customer_id);
 ALTER TABLE Spendings ADD CONSTRAINT FK_Spendings_Products FOREIGN KEY (product_barcode) REFERENCES Products (product_barcode);
